c16ca9cf6be7fee0f8f096533e4304738bd8362b
Dzung Nguyen <dung.nguyenvan@vti.com.vn>
2020-04-28 17:36:12 +0700

    sigle web server

--- example.tf
+++ example.tf
@@ -6,8 +6,19 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-07c1207a9d40bc3bd"
   instance_type = "t2.micro"
-
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
   tags = {
     Name = "terraform-example"
+  }
+  
+   ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
